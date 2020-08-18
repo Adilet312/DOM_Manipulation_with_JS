e79cec7c5b15c95893e65e052439286714b5cde3
Adilet312 <adiletm@hotmail.com>
2020-08-17 19:23:40 -0700

    Add overlay with image and overlay with close event handler.

--- src/main.js
+++ src/main.js
@@ -7,19 +7,18 @@ import image4 from './assets/jeans4.jpeg';
 const generateImages = function(){
   let images = document.querySelector('.imageItems');
   let children = images.children;
-  console.log(children)
   for(let idx = 0; idx <children.length; idx++){
-    let image = document.createElement('img');
-    let number = Number(1+idx);
-    switch(number){
-      case 1 : image.setAttribute('src',`${image1}`);
-      break;
-      case 2 : image.setAttribute('src',`${image2}`);
-      break;
-      case 3 : image.setAttribute('src',`${image3}`);
-      break;
-      case 4 : image.setAttribute('src',`${image4}`);
-      break;
+  let image = document.createElement('img');
+  let number = Number(1+idx);
+  switch(number){
+    case 1 : image.setAttribute('src',`${image1}`);
+  break;
+    case 2 : image.setAttribute('src',`${image2}`);
+    break;
+    case 3 : image.setAttribute('src',`${image3}`);
+    break;
+    case 4 : image.setAttribute('src',`${image4}`);
+    break;
     }
 
     children[idx].appendChild(image);
@@ -30,6 +29,7 @@ const enlargeTarget = function(){
   let myPictures = document.querySelector(".imageItems");
   myPictures.addEventListener('click',function(event){
     if(event.target.tagName==='IMG'){
+      /*Div element*/
       let overlay = document.createElement('div');
       overlay.style.position = "absolute";
       overlay.style.top = window.pageYOffset + "px";
@@ -39,8 +39,32 @@ const enlargeTarget = function(){
       overlay.style.width = window.innerWidth + "px";
       overlay.style.height = window.innerHeight + "px";
 
+      /*Get image*/
+      let image_url = event.target.src;
+      /*Set image*/
+      let imgTag = document.createElement('img');
+      imgTag.setAttribute("src",`${image_url}`);
+      imgTag.style.position = "absolute";
+      imgTag.style.display = "block";
+      /*Button*/
+      let close = document.createElement('span');
+      close.innerHTML ="X";
+      close.style.color = "white";
+      close.style.fontSize = "20px";
+      close.style.position = "absolute";
+      close.style.top = "10px";
+      close.style.right = "10px";
+      overlay.appendChild(close);
+      overlay.appendChild(imgTag);
+      close.addEventListener('click',function(){
+        if(overlay){
+          overlay.parentNode.removeChild(overlay);
+        }
+      })
       document.querySelector('body').appendChild(overlay);
+
     }
-  },false)
-}
+  },false);
+};
+
 enlargeTarget();
